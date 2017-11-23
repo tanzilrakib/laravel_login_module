c573122c2c60c36bd512ea3edb0ab25f9aaed6f8
T. Rkb <tanzil.rakib@gmail.com>
2017-11-23 23:42:28 +0600

    accountkit init1

--- resources/views/login-view.blade.php
+++ resources/views/login-view.blade.php
@@ -126,6 +126,8 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+    <script src="https://sdk.accountkit.com/en_US/sdk.js"></script>
+    
     <script>
 
       // initialize Account Kit with CSRF protection
